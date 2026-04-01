b5b43156abc7f4248c5478f266ab9b9de0105797
desounun123-cmyk <desounun123@gmail.com>
2026-04-01 09:53:21 +0100

    Delete server/__tests__/jest.config.js

--- server/__tests__/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testPathIgnorePatterns: [
-    'helpers\\.js$',
-  ],
-  transform: {
-    '\\.[jt]sx?$': ['babel-jest', { sourceType: 'script' }],
-  },
-};
